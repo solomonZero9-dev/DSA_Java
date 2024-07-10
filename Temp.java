9367bbcd6541f29122b30d26da84b5699f829f9d
solomonZero9-dev <solomonzero9@gmail.com>
2024-07-10 22:43:07 +0530

    Minor Changes

---
+++ Temp.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Temp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input Temperature in C: ");
+        float tempC = sc.nextFloat();
+        float tempF = (tempC * 9/5) + 32;
+        System.out.printf("Temperature in F: "+ tempF);
+
+    }
+}
---
+++ TypeCasting.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float num = input.nextFloat();
+        // If input is int, aotumatically cast to float
+        // Left data type has to be larger than right side one
+        // int num = input.nextFloat(); will give error
+        // But this can be done by: int num = (int)a where a is float variable
+
+        System.out.println(num);
+    }
+}
