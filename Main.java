7531d776591e52328a3a280e5b30e71b0c30f784
solomonZero9-dev <solomonzero9@gmail.com>
2024-07-10 15:04:01 +0530

    Small changes

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World!");
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("How are you?");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLine());
     }
 }
